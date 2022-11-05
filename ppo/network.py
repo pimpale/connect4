2a865a6ec3bc248c2332b94d3936468c997f663a
gpi <gpimpale29@gmail.com>
2022-11-05 01:37:09 -0700

    ppo

---
+++ ppo/network.py
@@ -0,0 +1,299 @@
+import math
+import numpy as np
+import numpy.typing as npt
+import tensorflow as tf
+from tensor_annotations import axes
+import tensor_annotations.tensorflow as ttf
+from tensorflow import keras
+import env
+from typing import NewType, TypeAlias
+
+ActionAxis: TypeAlias = NewType('Actions', axes.Axis)
+
+# Hyperparameters
+BOARD_CONV_FILTERS = 8
+HIDDEN_LAYER_SIZE = 50
+
+LR = 1e-5  # Lower lr stabilises training greatly
+GAMMA = 0.99
+
+# PPO2
+ACTOR_PPO_LOSS_CLIPPING=0.2
+PPO_TRAINING_EPO = 5
+# H stands for entropy here
+H_TARGET = 0.1
+
+class PPOAgent:
+    def __init__(self, width:int, height:int):
+        self.board_width = width
+        self.board_height = height
+        self.actor = self.__build_actor()
+        self.critic = self.__build_critic()
+        self._entropy_weight:float = np.log(width)
+
+
+    # Private
+    def __build_actor(self):
+        # 2D Matrix of int8 inputs
+        feature_board = keras.layers.Input((self.board_height, self.board_width), dtype=ttf.int8)
+
+        # convolve the board so that the network can focus on key features
+        convolved_board = keras.layers.Conv2D(BOARD_CONV_FILTERS, (4, 4), activation="relu")(feature_board )
+
+        # flatten the convolved board and concatenate it with other data
+        # this will be used as input for the dense hidden layers
+        hidden_layer_in = keras.layers.Concatenate()(
+            keras.layers.Flatten()(convolved_board),
+        )
+        # now 2 layers of hidden board size
+        hidden_layer0_out = keras.layers.Dense(
+            shape=HIDDEN_LAYER_SIZE, activation='relu')(hidden_layer_in)
+        hidden_layer1_out = keras.layers.Dense(
+            shape=HIDDEN_LAYER_SIZE, activation='relu')(hidden_layer0_out)
+        hidden_layer2_out = keras.layers.Dense(
+            shape=HIDDEN_LAYER_SIZE, activation='relu')(hidden_layer1_out)
+
+        action_probs = keras.layers.Dense(self.board_width, activation='softmax')(hidden_layer2_out)
+
+        # oldpolicy probs is the vector of predictions made by the old actor model
+        oldpolicy_probs = keras.layers.Input(shape=self.board_width)
+        # is a 1-hot vector cooresponding to the chosen_action (which may be different than the argmax since we add gumbel noise)
+        action_chosen = keras.layers.Input(shape=self.board_width)
+
+        # advantage captures how better an action is compared to the others at a given state
+        # https://theaisummer.com/Actor_critics/
+        advantage = keras.layers.Input(shape=(1,))
+
+        # Needed for entropy regularization
+        entropy_weight = keras.layers.Input(shape=(1,))
+
+        model = keras.Model(
+            inputs=[
+                # ppo features
+                advantage,
+                oldpolicy_probs,
+                action_chosen,
+                entropy_weight,
+                # real features
+                feature_board,
+            ],
+            outputs=[action_probs]
+        )
+
+
+        def actor_ppo_loss(
+            newpolicy_probs: ttf.Tensor2[ttf.float32, ActionAxis, axes.Batch],
+            oldpolicy_probs: ttf.Tensor2[ttf.float32, ActionAxis, axes.Batch],
+            action_chosen: ttf.Tensor2[ttf.float32, ActionAxis, axes.Batch],
+            advantage: ttf.Tensor1[ttf.float32,axes.Batch],
+            entropy_weight: ttf.Tensor1[ttf.float32, axes.Batch],
+        ):
+            # Reward
+            def r(
+                pi_new:ttf.Tensor2[ttf.float32, ActionAxis, axes.Batch],
+                pi_old:ttf.Tensor2[ttf.float32, ActionAxis, axes.Batch],
+                acts:ttf.Tensor2[ttf.float32, ActionAxis, axes.Batch]
+            ) -> ttf.Tensor1[ttf.float32, axes.Batch]:
+                pi_new_prob:ttf.Tensor1[ttf.float32, axes.Batch] = tf.reduce_sum(tf.multiply(pi_new, acts), axis=1)
+                pi_old_prob:ttf.Tensor1[ttf.float32, axes.Batch] = tf.reduce_sum(tf.multiply(pi_old, acts), axis=1)
+                return pi_new_prob / pi_old_prob
+
+            # reward 
+            rv = r(newpolicy_probs, oldpolicy_probs, action_chosen)
+
+            # PPO2 Loss Clipping
+            ppo2loss = tf.minimum(rv, tf.clip_by_value(rv, 1-ACTOR_PPO_LOSS_CLIPPING, 1+ACTOR_PPO_LOSS_CLIPPING)) * advantage
+
+            # Dual Loss (Unsure what the advantage of this is???)
+            dual_loss = tf.where(tf.less(advantage, 0.), tf.maximum(ppo2loss, 3. * advantage), ppo2loss)
+
+            # Calculate Entropy (how uncertain the prediction is)
+            entropy = -tf.reduce_sum(tf.multiply(newpolicy_probs, tf.math.log(newpolicy_probs)), axis=1, keepdims=True)
+
+            # actor loss
+            return -tf.reduce_sum(dual_loss) - entropy_weight * entropy
+
+        model.add_loss(actor_ppo_loss(
+          newpolicy_probs=action_probs,
+          oldpolicy_probs=oldpolicy_probs,
+          action_chosen=action_chosen,
+          advantage=advantage,
+          entropy_weight=entropy_weight
+        ))
+
+        model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=LR),
+        )
+
+        return model
+
+    # Private
+    # The critic attempts to learn the advantage
+    def __build_critic(self):
+        # 2D Matrix of int8 inputs
+        feature_board = keras.layers.Input((self.board_height, self.board_width), dtype=ttf.int8)
+
+        # convolve the board so that the network can focus on key features
+        convolved_board = keras.layers.Conv2D(BOARD_CONV_FILTERS, (4, 4), activation="relu")(feature_board)
+
+        # flatten the convolved board and concatenate it with other data
+        # this will be used as input for the dense hidden layers
+        hidden_layer_in = keras.layers.Concatenate()(
+            keras.layers.Flatten()(convolved_board),
+        )
+        # now 2 layers of hidden board size
+        hidden_layer0_out = keras.layers.Dense(
+            shape=HIDDEN_LAYER_SIZE, activation='relu')(hidden_layer_in)
+        hidden_layer1_out = keras.layers.Dense(
+            shape=HIDDEN_LAYER_SIZE, activation='relu')(hidden_layer0_out)
+        hidden_layer2_out = keras.layers.Dense(
+            shape=HIDDEN_LAYER_SIZE, activation='relu')(hidden_layer1_out)
+
+        value = keras.layers.Dense(1, activation='linear')(hidden_layer2_out)
+
+        model = keras.Model(
+            inputs=[feature_board],
+            outputs=[value]
+        )
+
+        model.compile(optimizer=keras.optimizers.Adam(learning_rate=LR), loss='mse')
+
+        return model
+
+    def save(self, actor_path:str, critic_path:str):
+        self.actor.save_weights(actor_path)
+        self.critic.save_weights(critic_path)
+
+    def load(self, actor_path:str, critic_path:str):
+        self.actor.load_weights(actor_path)
+        self.critic.load_weights(critic_path)
+
+    def get_network_params(self):
+        return [
+          self.actor.get_weights(),
+          self.critic.get_weights(),
+        ]
+
+    def set_network_params(self, weights):
+        self.actor.set_weights(weights[0])
+        self.critic.set_weights(weights[1])
+
+    def predict(
+        self,
+        observation_batch: list[env.Observation],
+    ):
+        batch_len = len(observation_batch)
+
+        # Convert state batch into correct format
+        board_batched = np.zeros((batch_len, self.board_height, self.board_height))
+        for i, (o,) in enumerate(observation_batch):
+            board_batched[i] = o
+
+        # Create other PPO2 things (needed for training, but during inference we dont care)
+        advantage_batched = np.zeros((batch_len, 1))
+        oldpolicy_probs_batched = np.zeros((batch_len, self.board_width))
+        chosen_action_batched = np.zeros((batch_len, self.board_width))
+        entropy_weight_batched = np.zeros((batch_len, 1))
+
+        p = self.actor(
+            [
+                # Dummy
+                advantage_batched,
+                oldpolicy_probs_batched,
+                chosen_action_batched,
+                entropy_weight_batched,
+                # Real
+                board_batched,
+            ],
+        )
+        return p
+
+    def train(
+        self,
+        observation_batch: list[env.Observation],
+        action_batch: list[env.Action],
+        advantage_batch:list[float],
+        old_prediction_batch: list[npt.NDArray[np.float32]],
+        base_step:int
+    ):
+        batch_len = len(observation_batch)
+        assert batch_len == len(action_batch)
+        assert batch_len == len(advantage_batch) 
+        assert batch_len == len(old_prediction_batch)
+
+        # Convert state batch into correct format
+        board_batched = np.zeros((batch_len, self.board_height, self.board_height))
+        for i, (o,) in enumerate(observation_batch):
+            board_batched[i] = o
+
+        # Create other PPO2 things (needed for training, but during inference we dont care)
+        advantage_batched  = np.reshape(advantage_batch, (batch_len, 1))
+        oldpolicy_probs_batched = np.reshape(old_prediction_batch, (batch_len, self.board_width))
+        chosen_action_batched  = np.reshape(action_batch, (batch_len, self.board_width))
+
+        entropy_weight = np.full((batch_len, 1), self._entropy_weight);
+
+        class PrintLoss(keras.callbacks.Callback):
+            def __init__(self, base_step:int, name:str):
+                self.base_step = base_step
+                self.name = name
+            def on_epoch_end(self, epoch, logs):
+                tf.summary.scalar(self.name, logs['loss'], step=self.base_step+epoch)
+
+        # Train Actor
+        self.actor.fit(
+            [
+                # Required to compute loss
+                advantage_batched,
+                oldpolicy_probs_batched,
+                chosen_action_batched,
+                entropy_weight,
+                # Real values
+                board_batched,
+            ],
+            epochs=PPO_TRAINING_EPO,
+            callbacks=[PrintLoss(base_step, 'actor_loss')]
+        )
+
+        # Train Critic
+        self.critic.fit(
+            [board_batched],
+            advantage_batched,
+            epochs=PPO_TRAINING_EPO,
+            callbacks=[PrintLoss(base_step, 'critic_loss')]
+        )
+
+        p_batch = np.clip(oldpolicy_probs_batched, ACTOR_PPO_LOSS_CLIPPING, 1. - ACTOR_PPO_LOSS_CLIPPING)
+        H = np.mean(np.sum(-np.log(p_batch) * p_batch, axis=1))
+        g = H - H_TARGET
+        self._entropy_weight -= LR * float(g) * 0.1
+
+        return PPO_TRAINING_EPO
+
+    def get_entropy_weight(self) -> float:
+        return self._entropy_weight
+
+    # computes advantage
+    def compute_advantage(
+        self,
+        state_batch: list[env.Observation],
+        reward_batch: list[env.Reward],
+        terminal: bool
+    ) -> list[float]:
+        assert len(state_batch) == len(reward_batch)
+
+        batch_len = len(state_batch)
+        R_batch = np.zeros(len(reward_batch))
+
+        if terminal:
+            R_batch[-1] = 0  # terminal state
+        else:
+            # calculate the advantage at the end
+            v_batch = self.critic([state_batch[-1]])
+            R_batch[-1] = v_batch[0]
+
+        # Use GAMMA to decay the advantage 
+        for t in reversed(range(batch_len- 1)):
+            R_batch[t] = reward_batch[t] + GAMMA * R_batch[t + 1]
+
+        return list(R_batch)
---
+++ ppo/train.py
@@ -0,0 +1,188 @@
+from io import TextIOWrapper
+import multiprocessing as mp
+import numpy as np
+import numpy.typing as npt
+import logging
+import tensorflow as tf
+import os
+import sys
+import network
+import env
+
+BOARD_XSIZE=7
+BOARD_YSIZE=6
+DIMS=(BOARD_YSIZE,BOARD_XSIZE)
+
+NUM_AGENTS = 16
+TRAIN_SEQ_LEN = 1000  # take as a train batch
+TRAIN_EPOCH = 500000
+MODEL_SAVE_INTERVAL = 100
+RANDOM_SEED = 42
+
+SUMMARY_DIR = './summary'
+MODEL_DIR = './models'
+TRAIN_TRACES = './train/'
+TEST_LOG_FOLDER = './test_results/'
+LOG_FILE = SUMMARY_DIR + '/log'
+
+# create result directory
+if not os.path.exists(SUMMARY_DIR):
+    os.makedirs(SUMMARY_DIR)
+
+def testing(epoch:int, nn_actor_model_path:str, nn_critic_model_path:str, log_file:TextIOWrapper):
+    # clean up the test results folder
+    os.system('rm -r ' + TEST_LOG_FOLDER)
+    #os.system('mkdir ' + TEST_LOG_FOLDER)
+    if not os.path.exists(TEST_LOG_FOLDER):
+        os.makedirs(TEST_LOG_FOLDER)
+    # run test script
+    os.system(f'python test.py {nn_actor_model_path} {nn_critic_model_path}')
+    # append test performance to the log
+    rewards_list:list[float] = []
+    entropies_list:list[float] = []
+    test_log_files = os.listdir(TEST_LOG_FOLDER)
+    for test_log_file in test_log_files:
+        reward, entropy = [], []
+        with open(TEST_LOG_FOLDER + test_log_file, 'rb') as f:
+            for line in f:
+                parse = line.split()
+                try:
+                    entropy.append(float(parse[-2]))
+                    reward.append(float(parse[-1]))
+                except IndexError:
+                    break
+        rewards_list.append(np.mean(reward[1:]))
+        entropies_list.append(np.mean(entropy[1:]))
+    rewards = np.array(rewards_list)
+    rewards_min = np.min(rewards)
+    rewards_5per = np.percentile(rewards, 5)
+    rewards_mean = np.mean(rewards)
+    rewards_median = np.percentile(rewards, 50)
+    rewards_95per = np.percentile(rewards, 95)
+    rewards_max = np.max(rewards)
+    log_file.write(str(epoch) + '\t' +
+                   str(rewards_min) + '\t' +
+                   str(rewards_5per) + '\t' +
+                   str(rewards_mean) + '\t' +
+                   str(rewards_median) + '\t' +
+                   str(rewards_95per) + '\t' +
+                   str(rewards_max) + '\n')
+    log_file.flush()
+    return rewards_mean, np.mean(entropies_list)
+        
+def central_agent(net_params_queues, exp_queues):
+    assert len(net_params_queues) == NUM_AGENTS
+    assert len(exp_queues) == NUM_AGENTS
+
+    # TODO: restore neural net parameters
+    actor = network.PPOAgent(BOARD_XSIZE, BOARD_YSIZE)
+
+    # Get Writer
+    writer = tf.summary.create_file_writer(SUMMARY_DIR);
+
+    step=0
+
+    with open(LOG_FILE + '_test.txt', 'w') as test_log_file, writer.as_default():
+        for epoch in range(TRAIN_EPOCH):
+            # synchronize the network parameters of work agent
+            actor_net_params = actor.get_network_params()
+            for i in range(NUM_AGENTS):
+                net_params_queues[i].put(actor_net_params)
+
+            s_batch:list[env.Observation] = []
+            a_batch:list[npt.NDArray[np.float32]] = []
+            p_batch:list[npt.NDArray[np.float32]]  = []
+            v_batch:list[float] = []
+            for i in range(NUM_AGENTS):
+                s_, a_, p_, g_ = exp_queues[i].get()
+                s_batch += s_
+                a_batch += a_
+                p_batch += p_
+                v_batch += g_
+
+            # Train Actor
+            steps_trained = actor.train(s_batch, a_batch, v_batch, p_batch, step)
+            step += steps_trained
+
+            if epoch % MODEL_SAVE_INTERVAL == 0:
+                # Save the neural net parameters to disk.
+                actor_path = f"{SUMMARY_DIR}/nn_model_ep_{epoch}_actor.ckpt"
+                critic_path = f"{SUMMARY_DIR}/nn_model_ep_{epoch}_critic.ckpt"
+                save_path = actor.save(actor_path, critic_path)
+
+                # Write to Log File
+                avg_reward, avg_entropy = testing(epoch, actor_path, critic_path, test_log_file)
+                tf.summary.scalar('avg_reward', avg_reward, step=step)
+                tf.summary.scalar('avg_entropy', avg_entropy, step=step)
+                tf.summary.scalar('entropy_weight', actor.get_entropy_weight(), step=step)
+
+
+
+def agent(agent_id, net_params_queue, exp_queue):
+    e = env.Env(agent_id)
+    actor = network.PPOAgent(BOARD_XSIZE, BOARD_YSIZE)
+
+    # initial synchronization of the network parameters from the coordinator
+    actor_net_params = net_params_queue.get()
+    actor.set_network_params(actor_net_params)
+
+    for epoch in range(TRAIN_EPOCH):
+        obs = e.reset()
+        s_batch:list[env.Observation] = []
+        a_batch:list[env.Action] = []
+        p_batch:list[npt.NDArray[np.float32]]  = []
+        r_batch:list[env.Reward] = []
+        done = True
+        for step in range(TRAIN_SEQ_LEN):
+            s_batch.append(obs)
+
+            action_prob = actor.predict([obs])[0]
+
+            # gumbel noise
+            noise = np.random.gumbel(size=len(action_prob))
+            chosen_vid_bitrate_idx = np.argmax(np.log(action_prob) + noise)
+
+            obs, reward, done, info = e.step(chosen_vid_bitrate_idx)
+
+            a_batch.append(action_vec)
+            r_batch.append(reward)
+            p_batch.append(action_prob)
+            if done:
+                break
+        v_batch = actor.compute_advantage(s_batch, r_batch, done)
+        exp_queue.put([s_batch, a_batch, p_batch, v_batch])
+
+        actor_net_params = net_params_queue.get()
+        actor.set_network_params(actor_net_params)
+
+def main():
+    np.random.seed(RANDOM_SEED)
+
+    # inter-process communication queues
+    net_params_queues = []
+    exp_queues = []
+    for i in range(NUM_AGENTS):
+        net_params_queues.append(mp.Queue(1))
+        exp_queues.append(mp.Queue(1))
+
+    # create a coordinator and multiple agent processes
+    # (note: threading is not desirable due to python GIL)
+    coordinator = mp.Process(target=central_agent,
+                             args=(net_params_queues, exp_queues))
+    coordinator.start()
+
+    agents = []
+    for i in range(NUM_AGENTS):
+        agents.append(mp.Process(target=agent,
+                                 args=(i,
+                                       net_params_queues[i],
+                                       exp_queues[i])))
+    for i in range(NUM_AGENTS):
+        agents[i].start()
+
+    # wait unit training is done
+    coordinator.join()
+
+
+if __name__ == '__main__':
+    main()
